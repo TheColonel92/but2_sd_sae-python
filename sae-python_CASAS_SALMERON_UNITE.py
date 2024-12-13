3c8d1e6f57d32508ac05465968b261814a20c3cb
InnateAlloy6 <innatealloy6@gmail.com>
2024-12-13 11:39:19 +0100

    Update sae-python_CASAS_SALMERON_UNITE.py

--- sae-python_CASAS_SALMERON_UNITE.py
+++ sae-python_CASAS_SALMERON_UNITE.py
@@ -2,11 +2,18 @@ import ucimlrepo
 
 from ucimlrepo import fetch_ucirepo 
   
+# Installer le package ucimlrepo
+#pip install ucimlrepo
+
+# Importer les données  
+from ucimlrepo import fetch_ucirepo 
+  
 # Récupérer l'ensemble de données
 statlog_german_credit_data = fetch_ucirepo(id=144) 
   
 # Les données (sous forme de dataframes pandas) 
 X = statlog_german_credit_data.data.features 
 y = statlog_german_credit_data.data.targets 
+
 
 print(statlog_german_credit_data.variables[['name','description']])
