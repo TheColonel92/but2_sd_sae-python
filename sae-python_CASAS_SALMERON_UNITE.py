379384f4e945cc8e57734dd54938ca608870c684
InnateAlloy6 <innatealloy6@gmail.com>
2024-12-13 11:37:33 +0100

    Update sae-python_CASAS_SALMERON_UNITE.py

--- sae-python_CASAS_SALMERON_UNITE.py
+++ sae-python_CASAS_SALMERON_UNITE.py
@@ -1,1 +1,10 @@
 import ucimlrepo
+
+from ucimlrepo import fetch_ucirepo 
+  
+# Récupérer l'ensemble de données
+statlog_german_credit_data = fetch_ucirepo(id=144) 
+  
+# Les données (sous forme de dataframes pandas) 
+X = statlog_german_credit_data.data.features 
+y = statlog_german_credit_data.data.targets 
